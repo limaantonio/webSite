2dc5bbba85426bd0cf7c0dbe7074415da6c9c025
limaantonio <limaantoniocarlos20@gmail.com>
2020-11-19 23:36:06 -0300

    add: signUp

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,13 +1,14 @@
 import Head from 'next/head';
 import Link from 'next/link';
 
-import "../styles/main.css";
+import "../../styles/main.css";
 
-import Input from '../components/Input';
+import Input from '../../components/Input';
 
-const Login = () => {
+const SignUp = () => {
   return (
-    <div className="h-auto sm:flex w-full ">
+    <div className="h-auto sm:flex w-full "
+    style={{ background: "url(./background.svg) no-repeat center/cover" }}>
         <div className="sm:flex sm:flex-col hidden  items-end p-2 sm:w-1/2 w-full min-h-screen justify-center ">
         <div className=" flex flex-col justify-end items-end">
          
@@ -18,12 +19,16 @@ const Login = () => {
       <div className="flex flex-col  items-center sm:w-1/2 w-full h-screen justify-center  ">
     
         <form className="flex flex-col items-center  my-10 w-10/12">
-          <Input name={"Usuário"}/>
-          <Input name={"Senha"}/>
-          <button className="bg-green w-4/12 rounded-3xl h-10 mt-6 ">Entrar</button>
+          <Input name={"Nome"}/>
+          <Input name={"E-mail"}/>
+          <Input name={"Endereço"}/>
+          <Input name={"CPF"}/>
+          <Input name={"RG"}/>
+          <Input name={"Telefone"}/>
+          <button className="bg-green w-4/12 rounded-3xl h-10 mt-6 ">Cadastrar</button>
           <div className="flex flex-col mt-8">
             <Link className="text-green" href="">Esqueceu a senha?</Link>
-            <Link className="text-green" href="/signup">Crie sua conta</Link>
+            <Link className="text-green" href="">Crie sua conta</Link>
           </div>
           </form>
         </div>
@@ -31,15 +36,4 @@ const Login = () => {
   );
 }
 
-export default function Home() {
-  return (
-    <div  className="flex flex-col flex-1"
-    style={{ background: "url(./background.svg) no-repeat center/cover" }}>
-      <Head>
-        <title>Login</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Login/>
-    </div>
-  )
-}
+export default SignUp;
